9fd7b5b53053e951bb06fd5b19c4d98ccc1b0338
adamawoolley <adamawoolley@gmail.com>
2019-02-20 17:10:16 -0800

    Create client.py

---
+++ client.py
@@ -0,0 +1,38 @@
+from sys import argv, exit
+import socket
+from getpass import getpass
+
+def client(host, port):
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        client.connect((host, port))
+
+        output = client.recv(1024)
+
+        command = input(output.decode('utf-8'))
+        while command != 'exit':
+                client.send(bytes(command, 'utf-8'))
+                output = client.recv(4096)
+                command = input(output.decode('utf-8'))
+
+def client_setup():
+        host = ''
+        port = 0
+
+        if '-s' in argv:
+                host = argv[argv.index('-s') + 1]
+        elif '--server' in argv:
+                host = argv[argv.index('--server') + 1]
+        else:
+                exit()
+        if '-p' in argv:
+                port = int(argv[argv.index('-p') + 1])
+        elif '--port' in argv:
+                port = int(argv[argv.index('--port') + 1])
+        else:
+                exit()
+
+        client(host, port)
+
+if __name__ == '__main__':
+        client_setup()
